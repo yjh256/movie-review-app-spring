a3b0dbeb5195519ba1314c1f8602236c239a59c3
YoonJiHwan <yjh256@snu.ac.kr>
2021-04-20 15:54:44 +0900

    Initial commit

---
+++ src/main/java/com/yjh256/moviereviewappspring/MovieReviewAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.yjh256.moviereviewappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewAppSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yjh256/moviereviewappspring/MovieReviewAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjh256.moviereviewappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
